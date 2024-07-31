078d521fb285dde83ccd2b5fab65ebbee93ce5a4
coin-123 <isobednlemchukwu@gmail.com>
2024-07-30 23:49:11 -0700

    made changes a nd added javascript commit

---
+++ asset/js/blog.js
@@ -0,0 +1,74 @@
+const carrd = document.querySelector("#card1");
+
+carrd.addEventListener("click", e => {
+    e.preventDefault();
+
+    const blogcontent = document.querySelector(".this-is-for-card1").style.display="block";
+    const blogpage = document.querySelector(".mini").style.display="none";
+})
+// console.log(carrd);
+
+const carrd2 = document.querySelector("#card2");
+
+carrd2.addEventListener("click", e => {
+    e.preventDefault();
+
+    const blogcontent = document.querySelector(".this-is-for-card2").style.display="block";
+    const blogpage = document.querySelector(".mini").style.display="none"
+})
+const carrd3 = document.querySelector("#card3");
+
+carrd3.addEventListener("click", e => {
+    e.preventDefault();
+
+    const blogcontent = document.querySelector(".this-is-for-card3").style.display="block";
+    const blogpage = document.querySelector(".mini").style.display="none"
+})
+const carrd4 = document.querySelector("#card4");
+
+carrd4.addEventListener("click", e => {
+    e.preventDefault();
+
+    const blogcontent = document.querySelector(".this-is-for-card4").style.display="block";
+    const blogpage = document.querySelector(".mini").style.display="none"
+})
+const carrd5 = document.querySelector("#card5");
+
+carrd5.addEventListener("click", e => {
+    e.preventDefault();
+
+    const blogcontent = document.querySelector(".this-is-for-card5").style.display="block";
+    const blogpage = document.querySelector(".mini").style.display="none"
+})
+const carrd6 = document.querySelector("#card6");
+
+carrd6.addEventListener("click", e => {
+    e.preventDefault();
+
+    const blogcontent = document.querySelector(".this-is-for-card6").style.display="block";
+    const blogpage = document.querySelector(".mini").style.display="none"
+})
+const carrd7 = document.querySelector("#card7");
+
+carrd7.addEventListener("click", e => {
+    e.preventDefault();
+
+    const blogcontent = document.querySelector(".this-is-for-card7").style.display="block";
+    const blogpage = document.querySelector(".mini").style.display="none"
+})
+const carrd8 = document.querySelector("#card8");
+
+carrd8.addEventListener("click", e => {
+    e.preventDefault();
+
+    const blogcontent = document.querySelector(".this-is-for-card8").style.display="block";
+    const blogpage = document.querySelector(".mini").style.display="none"
+})
+const carrd9 = document.querySelector("#card9");
+
+carrd9.addEventListener("click", e => {
+    e.preventDefault();
+
+    const blogcontent = document.querySelector(".this-is-for-card9").style.display="block";
+    const blogpage = document.querySelector(".mini").style.display="none"
+})
